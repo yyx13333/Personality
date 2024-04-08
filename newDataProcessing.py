d105c948ae0d52889c9a65b664b235e1445aad41
yyx13333 <119550532+yyx13333@users.noreply.github.com>
2024-04-08 21:23:44 +0800

    添加new

---
+++ newDataProcessing.py
@@ -0,0 +1,55 @@
+# 这个脚本将对CPED数据集进行处理，删除不用的信息（场景，说话人（将说话人归为A和B），情绪1，并将大五类分类）
+#   TODO:
+#       我们发现，CPED的数据集对话并不是一个每人说一句话的数据集
+#       我们首先需要对数据进行处理
+#
+import csv
+
+from tensorboardX import FileWriter
+
+with open('../data/CPED/train_split.csv', encoding='utf-8') as file:
+    reader = csv.reader(file)
+    with open('../data/myCPED/new_myCPED.csv', 'w', newline='',encoding='utf-8') as f:
+        myData = csv.writer(f)
+        dialogue_ID = ''
+        Utterance_ID = ''
+        count = 0
+        speaker = ''
+        newRow = []
+        emotion = ''
+        context = ''
+        cls = ''
+        flag = 0
+        for row in reader:
+            if flag == 0:
+                flag = 1
+                continue
+            if speaker == '':
+                speaker = row[3]
+            if dialogue_ID == '':
+                dialogue_ID = row[1]
+            if row[3] != speaker:
+                count = count + 1
+                newRow.append(speaker)
+                newRow.append(count)
+                newRow.append(emotion)
+                newRow.append(context)
+                myData.writerow(newRow)
+                context = ''
+                cls = ''
+                newRow = []
+            speaker = row[3]
+            emotion = row[15]
+            context = context + row[17] + '。'
+            for i in range(6, 11):
+                if row[i] == 'high':
+                    cls = cls + '1'
+                elif row[i] == 'low':
+                    cls = cls + '0'
+            if len(cls) < 5:
+                cls = 'UNK'
+            if dialogue_ID != row[1]:
+                dialogue_ID = row[1]
+                count = 0
+        f.close()
+    file.close()
