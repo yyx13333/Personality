c53dfa88de08a052239599cf1bc796b2c863f98d
yyx13333 <119550532+yyx13333@users.noreply.github.com>
2024-05-16 11:13:32 +0800

    修改

---
+++ DataLoader/caifen.py
@@ -0,0 +1,14 @@
+
+
+
+
+import csv
+
+
+
+
+
+
+
+with open("../../data/CPED/train_split.csv", "r", encoding="utf-8") as f:
+    reader = csv.reader(f)
--- DataLoader/fanyi.py
+++
@@ -1,5 +0,0 @@
-
-from translate import Translator  # noqa
-
-
-print(Translator(from_lang="Chinese",to_lang="English").translate('你好'))
